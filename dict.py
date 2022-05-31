8ed1bb6c087d5e9414c4f16bf57015e997060b77
adityafaujdar <106466960+adityafaujdar@users.noreply.github.com>
2022-05-31 17:25:58 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,9 @@
+ #Empty dictionary
+ new_dict = dict()
+ new_dict = {}
+ print(new_dict)
+{}
+
+ color = {"col1" : "Red", "col2" : "Green", "col3" : "Orange" }
+ color
+{'col2': 'Green', 'col3': 'Orange', 'col1': 'Red'}
