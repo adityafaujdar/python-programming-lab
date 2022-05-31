a5553bbf33bce02b7bf50d6b79d2d06f80f69f06
adityafaujdar <106466960+adityafaujdar@users.noreply.github.com>
2022-05-31 17:09:35 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,10 @@
+my_list = ['p','r','o','g','r','a','m','i','z']
+
+# elements from index 2 to index 4
+print(my_list[2:5])
+
+# elements from index 5 to end
+print(my_list[5:])
+
+# elements beginning to end
+print(my_list[:])
